e5340269de448ffaf5e712f63211732d973e2cf4
brancomartinezc <bmartinezcarpineti@gmail.com>
2024-04-09 14:30:06 -0300

    Agrego archivos de entrenamiento y cambio modelo

--- yolo/tracker.py
+++ yolo/tracker.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 TRACKER_DIR = os.path.dirname(os.path.abspath(__file__))
 VIDEOS_DIR = os.path.join(TRACKER_DIR, 'videos')
-video_name = 'vid4.mp4'
+video_name = 'vid6.mp4'
 
 video_path = os.path.join(VIDEOS_DIR, video_name)
 video_path_out = '{}_out.mp4'.format(video_path)
@@ -20,7 +20,7 @@ ret, frame = cap.read()
 H, W, _ = frame.shape
 out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
-model_path = os.path.join(TRACKER_DIR, 'runs', 'detect', 'train', 'weights', 'last.pt')
+model_path = os.path.join(TRACKER_DIR, 'model.pt')
 
 model = YOLO(model_path)
 
@@ -33,7 +33,7 @@ while ret:
     for result in results.boxes.data.tolist():
         x1, y1, x2, y2, score, class_id = result
 
-        if score > threshold:
+        if score > threshold: 
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
             cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
                         cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
