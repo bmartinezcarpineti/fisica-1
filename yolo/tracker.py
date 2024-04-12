ca1884809743c3428e499977e71af6b18f9bdfaf
brancomartinezc <bmartinezcarpineti@gmail.com>
2024-04-12 15:50:39 -0300

    agrego graficos de posicion, velocidad y aceleracion en X del objeto

--- yolo/tracker.py
+++ yolo/tracker.py
@@ -1,14 +1,16 @@
 import os
 import cv2
 from ultralytics import YOLO
+import matplotlib.pyplot as plt
 
 # files constants
-TRACKER_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-INPUT_VIDEOS_DIRECTORY = os.path.join(TRACKER_DIRECTORY, 'videos')
-INPUT_VIDEO_NAME = 'vid6.mp4'
-INPUT_VIDEO_PATH = os.path.join(INPUT_VIDEOS_DIRECTORY, INPUT_VIDEO_NAME)
+TRACKER_DIRECTORY_PATH = os.path.dirname(os.path.abspath(__file__))
+INPUT_VIDEOS_DIRECTORY_PATH = os.path.join(TRACKER_DIRECTORY_PATH, 'videos')
+INPUT_VIDEO_NAME = 'vid8.mp4'
+INPUT_VIDEO_PATH = os.path.join(INPUT_VIDEOS_DIRECTORY_PATH, INPUT_VIDEO_NAME)
 OUTPUT_VIDEO_PATH = '{}_out.mp4'.format(INPUT_VIDEO_PATH)
-YOLO_MODEL_PATH = os.path.join(TRACKER_DIRECTORY, 'model.pt')
+PLOTS_FILE_PATH = os.path.join(TRACKER_DIRECTORY_PATH, 'plots', 'object_plots.png')
+YOLO_MODEL_PATH = os.path.join(TRACKER_DIRECTORY_PATH, 'model.pt')
 
 if not os.path.exists(INPUT_VIDEO_PATH):
     print(f"Error: Video file '{INPUT_VIDEO_PATH}' not found.")
@@ -16,7 +18,7 @@ if not os.path.exists(INPUT_VIDEO_PATH):
 
 # video constants
 OBJECT_WIDTH_IN_METERS = 0.05
-OBJECT_WIDTH_IN_PIXELS = 110
+OBJECT_WIDTH_IN_PIXELS = 205
 PIXELS_PER_METER = OBJECT_WIDTH_IN_PIXELS / OBJECT_WIDTH_IN_METERS
 ORIGINAL_INPUT_VIDEO_FPS = 120
 TIME_DIFFERENCE = 1 / ORIGINAL_INPUT_VIDEO_FPS
@@ -36,6 +38,9 @@ object_speed_in_x = 0
 object_acceleration_in_x = 0
 previous_object_position_in_x = None
 previous_object_speed_in_x = None
+all_object_position_in_x = []
+all_object_speed_in_x = []
+all_object_acceleration_in_x = []
 
 # object detection loop
 while successful_video_reading:
@@ -47,17 +52,20 @@ while successful_video_reading:
 
         if detection_confidence_score > THRESHOLD_CONFIDENCE_SCORE:
             object_position_in_x = int((object_x1 + object_x2) / 2) # the position is represented by the center of the bounding box in the X axis
+            all_object_position_in_x.append(object_position_in_x / PIXELS_PER_METER)
 
             if previous_object_position_in_x is not None:
                 # speed calculation
                 traveled_distance_in_pixels = object_position_in_x - previous_object_position_in_x
                 traveled_distance_in_meters = traveled_distance_in_pixels / PIXELS_PER_METER
                 object_speed_in_x = traveled_distance_in_meters / TIME_DIFFERENCE
+                all_object_speed_in_x.append(object_speed_in_x)
             
             if previous_object_speed_in_x is not None:
                 # acceleration calculation
                 object_speed_in_x_difference = object_speed_in_x - previous_object_speed_in_x
                 object_acceleration_in_x = object_speed_in_x_difference / TIME_DIFFERENCE
+                all_object_acceleration_in_x.append(object_acceleration_in_x)
 
             # updates previous variables for next frame
             previous_object_position_in_x = object_position_in_x
@@ -65,9 +73,8 @@ while successful_video_reading:
 
             # draws bounding box and label
             cv2.rectangle(frame, (int(object_x1), int(object_y1)), (int(object_x2), int(object_y2)), (0, 255, 0), 4)
-            cv2.putText(frame, f"{object_speed_in_x:.2f} m/s {object_acceleration_in_x:.2f} m/s^2", 
-                        (int(object_x1), int(object_y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, 
-                        (0, 255, 0), 3, cv2.LINE_AA)
+            cv2.putText(frame, f"{(object_position_in_x / PIXELS_PER_METER):.2f} m, {object_speed_in_x:.2f} m/s, {object_acceleration_in_x:.2f} m/s^2", 
+                        (int(object_x1), int(object_y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
 
     output_video.write(frame)
     successful_video_reading, frame = input_video.read() # gets next frame
@@ -76,3 +83,22 @@ while successful_video_reading:
 input_video.release()
 output_video.release()
 cv2.destroyAllWindows()
+
+# plot
+plt.subplot(311)
+plt.plot(all_object_position_in_x, label="Postion in X")
+plt.ylabel("m")
+plt.legend()
+
+plt.subplot(312)
+plt.plot(all_object_speed_in_x, label="Speed in X")
+plt.ylabel("m/s")
+plt.legend()
+
+plt.subplot(313)
+plt.plot(all_object_acceleration_in_x, label="Acceleration in X")
+plt.ylabel("m/s^2")
+plt.xlabel("t (s)")
+plt.legend()
+
+plt.savefig(PLOTS_FILE_PATH)
