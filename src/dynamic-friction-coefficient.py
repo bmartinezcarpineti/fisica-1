3d093ffe8a1860bad99314daf1402680416d24d0
brancomartinezc <bmartinezcarpineti@gmail.com>
2024-06-12 11:27:26 -0300

    correcion ruta

--- src/dynamic-friction-coefficient.py
+++ src/dynamic-friction-coefficient.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 THIS_FILE_DIRECTORY_PATH = os.path.dirname(os.path.abspath(__file__))
-INPUT_CSV_PATH = os.path.join(THIS_FILE_DIRECTORY_PATH, 'data/without-elastic-data.csv')
+INPUT_CSV_PATH = os.path.join(THIS_FILE_DIRECTORY_PATH, 'data/friction-data.csv')
 GRAVITY = 9.81 # unit: m/s^s
 
 df = pd.read_csv(INPUT_CSV_PATH)
